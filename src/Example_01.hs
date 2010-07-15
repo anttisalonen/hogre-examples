e59b2747842faabe94637a740c0ea5bfea659575
Antti Salonen <ajsalonen@gmail.com>
2010-07-15 23:22:25 +0200

    adapt to ogre-0.1.0 - remove examples 2 and 3

--- src/Example_01.hs
+++ src/Example_01.hs
@@ -1,51 +1,77 @@
 module Main
 where
 
-import Graphics.Ogre.Ogre
-import Control.Exception
-import Control.Concurrent (threadDelay)
+import System.Exit
+import Control.Monad
 
-import Paths_hogre_examples
+import Graphics.Ogre.HOgre
+import Graphics.Ogre.Types
 
-renderLoop :: Float -> IO ()
-renderLoop f = do
-    renderOgre
-    setEntityPosition "obj1" (ogreloc f)
-    setLightPosition "light2" (Vector3 30 20 50)
-    threadDelay 10000
-    renderLoop (f + 0.008)
-
-ogreloc :: Float -> Vector3
-ogreloc f = Vector3 (30 + 20 * sin f) (8 + 2 * sin (0.2 * f)) (50 + 20 * cos f)
-
-plloc :: Float -> Vector3
-plloc f = Vector3 (30 + 40 * sin (4.0 * f)) 20 (50 + 40 * cos (4.0 * f))
-
+-- based on basic tutorial 6 from Ogre Wiki.
+-- http://www.ogre3d.org/tikiwiki/Basic+Tutorial+6&structure=Tutorials
 main :: IO ()
 main = do
-    resfile <- getDataFileName "resources.cfg"
-    let set = OgreSettings resfile True "Hogre Example 01" (Color 0.2 0.2 0.2) StencilAdditive [Generic]
-    let cam = Camera (Vector3 30.0 8.0 50.0) 0 (Vector3 135.0 40.0 50.0)
-    let yelcol = Color 1.0 1.0 0.0
-    let dircol = Color 0.25 0.25 0
-    let whitecol = Color 1 1 1
-    let spotrange = (0.0, degToRad 80.0)
-    let l1 = Light "light1" yelcol yelcol (SpotLight (Vector3  30 50.0 50) (Vector3  0.0  (-1.0)   0.0) spotrange)
-    let l2 = Light "light2" whitecol whitecol (PointLight (plloc 0))
-    let l3 = Light "light3" dircol dircol (DirectionalLight (Vector3 0 0 1))
-    let plane = Plane unitY 0.0 70.0 100.0 20 20 5.0 5.0 unitZ "WoodPallet"
-    let pl = Entity "ground" (Vector3 35.0 0.0 50.0) plane False (Vector3 1.0 1.0 1.0)
-    let lig = [l1, l2, l3]
-    let ents = [pl]
-    let sce = OgreScene cam ents lig
-    initOgre set
-    addScene sce
-    addEntity (Entity "obj1" (ogreloc 0) (StdMesh "robot.mesh" (YPR 0 0 0)) True (Vector3 0.20 0.20 0.20))
-    handle shutdown (renderLoop 0)
+  -- construct Ogre::Root
+  root <- root_new "plugins.cfg" "ogre.cfg" "Ogre.log"
 
-shutdown :: IOException -> IO ()
-shutdown e = do
-    print e
-    putStrLn "Shutting down..."
-    cleanupOgre
+  -- setup resources
+  root_addResourceLocation root "Media" "FileSystem" "Group" True
+
+  -- configure
+  -- show the configuration dialog and initialise the system
+  restored <- root_restoreConfig root
+  when (not restored) $ do
+    configured <- root_showConfigDialog root
+    when (not configured) $ exitWith (ExitFailure 1)
+  window <- root_initialise root True "Render Window" ""
+
+  -- set default mipmap level (some APIs ignore this)
+  root_getTextureManager root >>= \tmgr -> textureManager_setDefaultNumMipmaps tmgr 5
+
+  -- initialise all resource groups
+  resourceGroupManager_getSingletonPtr >>= resourceGroupManager_initialiseAllResourceGroups
+
+  -- create the scene manager, here a generic one
+  smgr <- root_createSceneManager1 root "DefaultSceneManager" ""
+
+  -- create and position the camera
+  cam <- sceneManager_createCamera smgr "PlayerCam"
+  camera_setPosition1 cam 0 0 80
+  camera_lookAt cam 0 0 (-300)
+  frustum_setNearClipDistance (toFrustum cam) 5
+
+  -- create one viewport, entire window
+  vp <- renderTarget_addViewport (toRenderTarget window) cam 0 0 0 1 1
+  bgcolour <- colourValue_new 0 0 0 1
+  viewport_setBackgroundColour vp bgcolour
+
+  -- Alter the camera aspect ratio to match the viewport
+  vpw <- viewport_getActualWidth vp
+  vph <- viewport_getActualHeight vp
+  frustum_setAspectRatio (toFrustum cam) (fromIntegral vpw / fromIntegral vph)
+
+  ogreHead <- sceneManager_createEntity2 smgr "ogrehead.mesh"
+  rootNode <- sceneManager_getRootSceneNode smgr
+  headNode <- sceneManager_createSceneNode smgr ""
+  node_addChild (toNode rootNode) (toNode headNode)
+  sceneNode_attachObject headNode (toMovableObject ogreHead)
+
+  -- set ambient light
+  colourValue_new 0.5 0.5 0.5 1.0 >>= sceneManager_setAmbientLight smgr
+
+  -- create a light
+  l <- sceneManager_createLight1 smgr "MainLight"
+  light_setPosition1 l 20 80 50
+
+  render window root
+
+  root_delete root
+
+render :: RenderWindow -> Root -> IO ()
+render window root = do
+  windowEventUtilities_messagePump
+  closed <- renderWindow_isClosed window
+  when (not closed) $ do
+      success <- root_renderOneFrame1 root
+      when success $ render window root
 
